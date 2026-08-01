cf197463e81ef774413008e1b71399ce7661bef7
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-08-01 15:27:05 +0600

    Revert "Update utility.js"
    This reverts commit 8dd516a231b9efa70a04a99f90ab2ea60958076e.

--- src/asset/utility.js
+++ src/asset/utility.js
@@ -1,40 +1,42 @@
-// PREFIXES
 export const LIST_PREFIX = "{{todolistzz}}";
 export const TODO_PREFIX = "{{todozz}}";
-export const ID_PREFIX = "#";
-export const CLASS_PREFIX = ".";
 
-// OPTIONS
 export const ACTIVE_LIST = "active-todoList";
 export const NOT_FIRST_TIME = TODO_PREFIX + "FreshStart";
+export const SIDEBAR_ID = "#sidebar";
+export const CONTENT_ID = "#content";
+export const MODAL_CLASS = ".modal";
+export const MODAL_CONTENT_CLASS = ".modal-content";
 
-// Attributes Value
-export const SIDEBAR = "sidebar";
-export const CONTENT = "content";
-export const MODAL = "modal";
-export const MODAL_CONTENT = "modal-content";
-export const ACTIONS_DIV = "actions";
-export const RADIO_BTN_DIV = "radio-btns";
-export const RADIO_BTN = "radio-btn";
-export const ADD_BTN = "add-btn";
-export const ACTION_BTN = "action-btn";
-export const LINK_BTN = "link";
-export const CLOSE_BTN = "close-btn";
-export const TITLE = "action-btn";
-export const PRIORITY = "priority";
-export const DATE = "date";
-export const LABEL = "label";
-export const NOTE = "note";
-export const DESC = "desc";
-export const LIST_TITLE = "list-title";
-export const LIST_HEADER = "list-header";
-export const TODO_LIST = "todo-list";
-export const TODOS = "todos";
-export const TODO = "todo";
-export const TODO_FORM = "todo-form";
-export const TODO_LIST_FORM = "todo-list-form";
-export const FORM_INPUTS = "form-inputs";
-export const FORM_SUBMIT = "form-submit";
-export const TEXT_BOX = "text-box";
-export const SELECT_BOX = "select-box";
-export const CHECKBOX = "checkbox";
+// let defaultTodoList = new TodoList(
+//   "My Todo List",
+//   "Description of My Todo List",
+// );
+// let defaultTodo = new Todo(
+//   "My Todo",
+//   defaultTodoList.id,
+//   "Description of My Todo.",
+//   "2026-07-24",
+//   "low",
+//   "#work",
+//   "this is a test",
+// );
+// let defaultTodoList2 = new TodoList(
+//   "My Todo List 2",
+//   "Description of My Todo List",
+// );
+// let defaultTodo2 = new Todo(
+//   "My Todo 2",
+//   defaultTodoList2.id,
+//   "Description of My Todo.",
+//   "2026-07-24",
+//   "low",
+//   "#work",
+//   "this is a test",
+// );
+// Store.setItems([
+//   defaultTodoList,
+//   defaultTodo,
+//   defaultTodoList2,
+//   defaultTodo2,
+// ]);
