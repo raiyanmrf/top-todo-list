905793ae69b49e5e07cc3933a34a79385a43c93b
Raiyan mrf <140499799+raiyanmrf@users.noreply.github.com>
2026-08-03 07:23:27 +0600

    Update todo-list.js
    REMOVE: note and label

--- src/todo-list.js
+++ src/todo-list.js
@@ -3,7 +3,6 @@ import { LIST_PREFIX } from "./asset/utility.js";
 export default class TodoList {
   constructor(title = "", desc = "") {
     this.title = title;
-    this.desc = desc;
     this.status = "active";
     this.id = LIST_PREFIX + crypto.randomUUID();
   }
